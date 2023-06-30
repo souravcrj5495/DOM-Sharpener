d96cee6a703542d0a3cda103ea0b0b7b4bb3666a
souravcrj5495 <souravcrj1995@gmail.com>
2023-06-30 20:45:11 +0530

    second commit

---
+++ mydom.js
@@ -0,0 +1,7 @@
+console.log("hathi my subhashree");
+var header = document.getElementById('header-title');
+console.log(header);
+header.style.borderBottom = 'solid 3px #000';
+var item1 = document.getElementById('item');
+item1.style.color= 'green';
+item1.style.fontWeight= 'bold';
